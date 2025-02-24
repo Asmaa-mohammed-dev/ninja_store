c950f0f6f27f52d54fb2459f6180bed7f3bb3566
Asmaa-elfeel <elfeelasmaa66@gmail.com>
2025-02-24 17:17:36 +0200

    lesson5

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -7,7 +7,6 @@ class NColors {
   static const Color primary = Color(0xFF4b68ff);
   static const Color secondary = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFb0c7ff);
-  static const Color accent1 = Color(0xFFb0c7ff);
 
   //gradient colors
   static const Gradient lineraGradient = LinearGradient(
