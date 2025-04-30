a205752715ad4b23647542c9ec2551a76e34d883
Asmaa-mohammed-dev <elfeelasmaa66@gmail.com>
2025-04-30 16:20:57 +0300

    lesson-29

---
+++ lib/features/shop/screens/sub_category/sub_categories.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:ninja_store/common/widgets/custom_shapes/appbar/appbar.dart';
+import 'package:ninja_store/common/widgets/images/n_rounde_image.dart';
+import 'package:ninja_store/common/widgets/products/product_card_horizontal.dart';
+import 'package:ninja_store/common/widgets/texts/section_heading.dart';
+import 'package:ninja_store/utils/constants/image_strings.dart';
+import 'package:ninja_store/utils/constants/sizes.dart';
+
+class SubCategoriesScreen extends StatelessWidget {
+  const SubCategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: NAppBar(title: Text('الطعام'), showBackArrow: true),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(NSizes.defaultSpace),
+            child: Column(
+              children: [
+                //Banner
+                NRoundedImage(
+                  width: double.infinity,
+
+                  imageUrl: NImages.prombanner3,
+                  applyImageRadius: true,
+                ),
+                const SizedBox(height: NSizes.spaceBtwSections),
+
+                //Sub Categories
+                Column(
+                  children: [
+                    //heading
+                    NsectionHeading(title: 'الدجاج'),
+                  ],
+                ),
+                SizedBox(height: NSizes.spaceBtwItems / 2),
+                SizedBox(
+                  height: 130,
+
+                  child: ListView.separated(
+                    itemCount: 4,
+                    scrollDirection: Axis.horizontal,
+                    separatorBuilder:
+                        (context, index) =>
+                            const SizedBox(width: NSizes.spaceBtwItems),
+                    itemBuilder: (context, index) => NProductCardHorizontal(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
