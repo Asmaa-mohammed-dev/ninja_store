00166c8a49b45cbf25fbe20433c7298e2bcc8e3a
Asmaa-mohammed-dev <elfeelasmaa66@gmail.com>
2025-05-01 11:49:31 +0300

    lesson-31

---
+++ lib/common/widgets/products/sortable/n_sortable_product.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:ninja_store/common/widgets/layouts/grid_layout.dart';
+import 'package:ninja_store/common/widgets/products/product_card_vertical.dart';
+import 'package:ninja_store/utils/constants/sizes.dart';
+
+class NSortableProducts extends StatelessWidget {
+  const NSortableProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //Dropdown
+        Directionality(
+          textDirection: TextDirection.rtl,
+          child: DropdownButtonFormField(
+            decoration: InputDecoration(prefixIcon: Icon(Iconsax.sort)),
+            isExpanded: true, // لتوسيع الـ dropdown بالعربي
+            alignment: Alignment.centerRight,
+            onChanged: (value) {},
+            items:
+                ['الاسم', 'أعلى سعر', 'أقل سعر', 'تخفيضات', 'الأجدد', 'الشعبية']
+                    .map(
+                      (option) => DropdownMenuItem(
+                        value: option,
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(option),
+                        ),
+                      ),
+                    )
+                    .toList(),
+          ),
+        ),
+        const SizedBox(height: NSizes.spaceBtwSections),
+
+        //Products
+        NGridLayout(
+          itemCount: 4,
+          itemBuilder: (_, index) => NProductCardVertical(),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/features/shop/screens/brands/all_brands.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ninja_store/common/widgets/custom_shapes/appbar/appbar.dart';
+import 'package:ninja_store/common/widgets/layouts/grid_layout.dart';
+import 'package:ninja_store/common/widgets/products/sortable/n_sortable_product.dart';
+import 'package:ninja_store/common/widgets/texts/section_heading.dart';
+import 'package:ninja_store/features/shop/screens/brands/brand_product.dart';
+import 'package:ninja_store/features/shop/screens/home/widgets/n_brand_card.dart';
+import 'package:ninja_store/utils/constants/sizes.dart';
+
+class AllBrandsScreen extends StatelessWidget {
+  const AllBrandsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: NAppBar(title: Text('العلامات الرائجة'), showBackArrow: true),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(NSizes.defaultSpace),
+            child: Column(
+              children: [
+                //Heading
+                NsectionHeading(title: 'العلامات الرائجة'),
+                SizedBox(height: NSizes.spaceBtwItems),
+
+                //Brands
+                NGridLayout(
+                  itemCount: 10,
+                  mainAxisExtent: 80,
+                  itemBuilder:
+                      (context, index) => NBrandCard(
+                        showBorder: true,
+                        onTap: () => Get.to(() => const BrandProducts()),
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
