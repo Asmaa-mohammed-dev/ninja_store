63b66e6e6dbc07620bf30eddac46657df5db4360
Asmaa-mohammed-dev <elfeelasmaa66@gmail.com>
2025-05-01 14:57:31 +0300

    lesson-32

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ninja_store/app.dart';
+import 'package:ninja_store/firebase_options.dart';
+  // Entry point of flutter app
+Future<void> main() async{
 
-void main() {
+  // Todo: Add Widgets Binding
+  // Todo: Init Local Storage
+  // Todo: Init Payment Mehtod
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+).then(Firebase value) => Get.put(AuthenticationRepository()),
+  // Todo: initialize Authentication
+  // Load all The Material Design / Themes / Localization / Bindings
   runApp(const App());
-  
 }
